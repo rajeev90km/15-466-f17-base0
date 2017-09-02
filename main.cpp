9fbd5f4fb748759d94b17ed5c865cc57f29e17be
Rajeev Mukundan <rajeev90km@gmail.com>
2017-09-02 17:35:56 -0400

    Base unit complete

--- main.cpp
+++ main.cpp
@@ -1,17 +1,37 @@
 #include "Draw.hpp"
 #include "GL.hpp"
 
-#include <SDL.h>
+#include <SDL2/SDL.h>
 #include <glm/glm.hpp>
 
 #include <chrono>
 #include <iostream>
+#include <queue>
+
+enum BaseColors{BLACK,GRAY,YELLOW,PURPLE,RED};
+
+glm::u8vec4 BaseColorValues[5] = {glm::u8vec4(0x3f, 0x3f, 0x3f, 0xff),glm::u8vec4(0xf2, 0xf2, 0xf2, 0xff),glm::u8vec4(0xff, 0xd9, 0x1e, 0xff),glm::u8vec4(0xaa, 0x61, 0xce, 0xff),glm::u8vec4(0xe7, 0x59, 0x26, 0xff)};
+
+class Base{
+    public:
+        float x1,y1,x2,y2;
+        BaseColors base_color;
+        glm::u8vec4 base_color_val;
+};
+
+
 
 int main(int argc, char **argv) {
+    
+    std::queue<int> missiles;
+    missiles.push(1);
+    
+    std::cout << missiles.front();
+    
 	//Configuration:
 	struct {
-		std::string title = "Game0: Tennis For One";
-		glm::uvec2 size = glm::uvec2(640, 480);
+		std::string title = "Game0: Color slide";
+		glm::uvec2 size = glm::uvec2(480, 640);
 	} config;
 
 	//------------  initialization ------------
@@ -70,15 +90,41 @@ int main(int argc, char **argv) {
 			std::cerr << "NOTE: couldn't set vsync (" << SDL_GetError() << ")." << std::endl;
 		}
 	}
+    
+    
 
 	//Hide mouse cursor (note: showing can be useful for debugging):
-	SDL_ShowCursor(SDL_DISABLE);
+//	SDL_ShowCursor(SDL_DISABLE);
 
 	//------------  game state ------------
 
 	glm::vec2 mouse = glm::vec2(0.0f, 0.0f);
 	glm::vec2 ball = glm::vec2(0.0f, 0.0f);
-	glm::vec2 ball_velocity = glm::vec2(0.5f, 0.5f);
+	glm::vec2 ball_velocity = glm::vec2(0.9f, -0.9f);
+    
+    const glm::vec2 start_pos = glm::vec2(-3.0f,-1.0f);
+    float init_base_height = -0.55f;
+    const int numUnits = 10;
+    Base baseUnits[numUnits];
+    
+    
+    //----------- Create Base Units ---------START
+    {
+
+        for(int i=0;i<numUnits;i++){
+            baseUnits[i].x1 = start_pos.x + (i*0.5f);
+            baseUnits[i].x2 = baseUnits[i].x1 + 0.5f;
+        
+            baseUnits[i].y1 = start_pos.y;
+            baseUnits[i].y2 = init_base_height;
+        
+            baseUnits[i].base_color = (BaseColors) (i%5);
+            baseUnits[i].base_color_val = BaseColorValues[baseUnits[i].base_color];
+        }
+    }
+     //----------- Create Base Units ---------END
+        
+        
 
 	//------------  game loop ------------
 
@@ -88,13 +134,18 @@ int main(int argc, char **argv) {
 		static SDL_Event evt;
 		while (SDL_PollEvent(&evt) == 1) {
 			//handle input:
-			if (evt.type == SDL_MOUSEMOTION) {
-				mouse.x = (evt.motion.x + 0.5f) / float(config.size.x) * 2.0f - 1.0f;
-				mouse.y = (evt.motion.y + 0.5f) / float(config.size.y) *-2.0f + 1.0f;
-			} else if (evt.type == SDL_MOUSEBUTTONDOWN) {
-				ball = mouse;
-				ball_velocity = glm::vec2(0.5f, 0.5f);
-			} else if (evt.type == SDL_KEYDOWN && evt.key.keysym.sym == SDLK_ESCAPE) {
+            
+            if(evt.type == SDL_MOUSEMOTION) {
+                    mouse.x = (evt.motion.x) / float(config.size.x) * 3.0f - 1.0f;
+                    mouse.y = 0.0f;
+            }
+//            if (evt.type == SDL_MOUSEBUTTONDOWN) {
+//                mouse_pressed = true;
+//			}
+//            else if(evt.type == SDL_MOUSEBUTTONUP){
+//                mouse_pressed = false;
+//            }
+            else if (evt.type == SDL_KEYDOWN && evt.key.keysym.sym == SDLK_ESCAPE) {
 				should_quit = true;
 			} else if (evt.type == SDL_QUIT) {
 				should_quit = true;
@@ -116,14 +167,21 @@ int main(int argc, char **argv) {
 		}
 
 		//draw output:
-		glClearColor(0.0, 0.0, 0.0, 0.0);
+		glClearColor(1,1,1,1);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 
 		{ //draw game state:
 			Draw draw;
-			draw.add_rectangle(mouse + glm::vec2(-0.1f,-0.1f), mouse + glm::vec2(0.1f, 0.1f), glm::u8vec4(0xff, 0xff, 0x00, 0xff));
-			draw.add_rectangle(ball + glm::vec2(-0.05f,-0.05f), ball + glm::vec2(0.05f, 0.05f), glm::u8vec4(0xff, 0x00, 0xff, 0xff));
+            
+            for(int i=0;i<numUnits;i++){
+                draw.add_rectangle(mouse+glm::vec2(baseUnits[i].x1,baseUnits[i].y1),mouse+glm::vec2(baseUnits[i].x2,baseUnits[i].y2),baseUnits[i].base_color_val);
+            }
+//			draw.add_rectangle(glm::vec2(-1.0f,-1.0f), glm::vec2(-0.5f,-0.55f), glm::u8vec4(0x3f, 0x3f, 0x3f, 0xff));
+//            draw.add_rectangle(glm::vec2(-0.5f,-1.0f), glm::vec2(0.0f,-0.55f), glm::u8vec4(0xf2, 0xf2, 0xf2, 0xff));
+//            draw.add_rectangle(glm::vec2(0.0f,-1.0f), glm::vec2(0.5f,-0.55f), glm::u8vec4(0xff, 0xd9, 0x1e, 0xff));
+//            draw.add_rectangle(glm::vec2(0.5f,-1.0f), glm::vec2(1.0f,-0.55f), glm::u8vec4(0xaa, 0x61, 0xce, 0xff));
+            draw.add_rectangle(ball + glm::vec2(-0.04f,-0.04f), ball + glm::vec2(0.07f, 0.04f), glm::u8vec4(0xff, 0x00, 0xff, 0xff));
 			draw.draw();
 		}
 
